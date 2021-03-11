ffe7ac6b519dfa4c1d601a52c49a39c9b4ea481d
Sanjiv chaudhary <sanjivchudhary416@gmail.com>
2021-03-11 21:50:14 +0545

    generated apple

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const grid= document.querySelector('.grid');
-const startButton= document.getElementById('#start');
+const startButton= document.getElementById('start');
 const score= document.getElementById('score');
-const squares=[];
+let squares=[];
 let currentSnake=[2,1,0];
 let direction =1;
+let width = 10
+let appleIndex= 0
+
 function createGrid(){
   // create 100 of these elements with a for loop
-    for(let i=0; i<400;i++){
+    for(let i=0; i<width*width;i++){
       // crate element
     const square=document.createElement('div')
       // add stying to these element
@@ -19,9 +22,21 @@ function createGrid(){
 }
 
 createGrid();
+
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move(){
+
+  if(
+     (currentSnake[0] + width >= width * width && direction ===width ) ||//if snake has hit botton
+    (currentSnake[0] % width ===width -1 && direction === 1) ||//if snake  has hit right wall
+      (currentSnake[0] % width === 0 && direction === -1) ||//if snake has hit left wall
+    (currentSnake[0] - width < 0 && direction === -width) || //if snake has  hit top wall
+    squares[currentSnake[0] + direction].classList.contains('snake') //if snake move to opposite
+   )
+
+return clearInterval(timerId)
+
   //remove last element fron current snake array
   const tail= currentSnake.pop()
   //remove stylig from last element
@@ -35,5 +50,41 @@ move();
 
 //const timerId= setInterval(function,time)
 
-const timerId=setInterval(move,1000)
-// clearInterval(timerId)
+let timerId=setInterval(move,1000)
+function generateApples(){
+  do{
+    //genetare a random number
+  appleIndex = Math.floor(Math.random()*squares.length)
+  console.log(appleIndex)
+  }while (squares[appleIndex].classList.contains('snake'))
+  squares[appleIndex].classList.add('apple')
+}
+
+generateApples()
+
+
+
+//keycode
+// 39 is right arrow
+// 38 is the up arrow
+// 37 is the left arrow
+// 40 is for the down arrow
+
+
+function control(e) {
+    if (e.keyCode === 39) {
+        console.log('right pressed')
+        direction = 1
+    } else if (e.keyCode === 38) {
+        console.log('up pressed')
+        direction = -width
+    } else if (e.keyCode === 37) {
+        console.log('left pressed')
+        direction = -1
+    } else if (e.keyCode === 40) {
+        console.log('down pressed')
+        direction = +width
+    }
+}
+document.addEventListener('keyup', control)
+// document.addEventListener('eventype',function)
